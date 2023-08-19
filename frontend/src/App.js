4d4497babffd8c28ada820ed2e2267db5efd2cd4
Ryan Li <ryan.lichangrui@gmail.com>
2023-08-19 11:35:46 -0400

    chakra setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,53 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+    //Data from backend
+    const [data, setData] = useState([]);
 
-  //Data from backend
-  const [data, setData] = useState([])
+    useEffect(() => {
+        getDatas();
+    }, []);
 
+    function getDatas() {
+        axios.get("http://127.0.0.1:5000/listusers").then(function (response) {
+            console.log(response.data);
+            setData(response.data);
+        });
+    }
 
-  useEffect(() => {
-    getDatas()
-  }, [])
+    const [inputs, setInputs] = useState();
 
-  function getDatas() {
-    axios.get('http://127.0.0.1:5000/listusers').then(function (response) {
-      console.log(response.data)
-      setData(response.data)
-    })
-  }
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs((prevInputs) => ({ ...prevInputs, [name]: value }));
+    };
 
-  const [inputs, setInputs] = useState();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(prevInputs => ({ ...prevInputs, [name]: value }))
-  }
+        console.log(inputs);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+        await axios
+            .post("http://127.0.0.1:5000/useradd", inputs)
+            .then(function (response) {
+                console.log(response.data);
+                setData(response.data);
+            });
+    };
 
-    console.log(inputs)
+    return (
+        <div>
+            {data}
 
-    await axios.post('http://127.0.0.1:5000/useradd', inputs).then(function (response) {
-      console.log(response.data)
-      setData(response.data);
-    })
-  }
-
-  return (
-    <div>
-      {data}
-
-      <form onSubmit={handleSubmit}>
-        <div>Submit photo here</div>
-        <input onChange={handleChange} />
-        <button type="submit">Add</button>
-      </form>
-
-    </div>
-  )
+            <form onSubmit={handleSubmit}>
+                <div>Submit photo here</div>
+                <input onChange={handleChange} />
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
 }
 
-export default App
+export default App;
